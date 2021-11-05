8ceb54a53627895d257f12f19eeab7fc89171dee
Rodrigo Mejia <rodrigoemdiaz@hotmail.com>
2021-11-05 13:14:02 -0700

    Initial Commit

---
+++ SoundBoard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SoundBoard
+//
+//  Created by RTodrigo Mejia on 11/5/21.
+//  Copyright © 2021 Rodrigo Mejia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
